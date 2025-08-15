3784a53341f846418ecb12f51c4185c8f3b8d379
muhammedfayiz122@gmail.com <muhammedfayiz122@gmail.com>
2025-08-15 07:18:59 +0530

    config file completed

--- document_portal/src/multi_document_chat/data_ingestion.py
+++ document_portal/src/multi_document_chat/data_ingestion.py
@@ -1,11 +1,50 @@
-class DocumentIngestor:
-    def __init__(self, document):
-        self.document = document
+from pathlib import Path
+from document_portal.exception.custom_exception import DocumentPortalException
+from document_portal.logger.cloud_logger import CustomLogger
+from document_portal.utils.model_loader import ModelLoader
+from uuid import uuid4
+import sys
+import os
 
-    def ingest_files(self):
+class DocumentIngestor:
+    def __init__(self, temp_dir, faiss_dir):
+        self.log = CustomLogger().get_logger(__name__)
+        self.temp_dir = temp_dir
+        os.makedirs(self.temp_dir, exist_ok=True)
+        self.faiss_dir = faiss_dir
+        os.makedirs(self.faiss_dir, exist_ok=True)
+        
+        # Session Managing
+        self.session_folder = f"session_{uuid4().hex}"
+        
+        model = 
+        
+        
+
+    def ingest_files(self, upload_files: Path) -> None:
+        """
+        Save the uploaded file in session and load it.
+
+        Args:
+            upload_files (Path): The path to the uploaded files.
+
+        Returns:
+            _type_: _description_
+        """
         try:
             pass
         except Exception as e:
-            print(f"An error occurred while ingesting files: {e}")
-            return None
+            self.log.error("Failed to initialize DocumentIngestor", error=str(e))
+            raise DocumentPortalException("Intialization error in DocumentIngestor", sys)
+            
         
+    def _create_retriever(self):
+        """
+        Create a retriever for the ingested documents.
+        """
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Failed to create retriever", error=str(e))
+            raise DocumentPortalException("Retrieval error in DocumentIngestor", sys)
+            
--- document_portal/utils/model_loader.py
+++ document_portal/utils/model_loader.py
@@ -1,2 +1,42 @@
+import sys
+from document_portal.exception.custom_exception import DocumentPortalException
+from document_portal.logger.cloud_logger import CustomLogger
+from dotenv import load_dotenv
+import os
+
+log = CustomLogger().get_logger(__name__)
+
 class ModelLoader():
-    pass
+    """
+    Utility class to load LLM models and embedding models.
+    """
+    def __init__(self):
+        
+        
+        load_dotenv()
+        self._validate_env()
+        
+    
+    def _validate_env(self):
+        """
+        Validate that necessary environment variables are set.
+        Ensure API keys and model names are provided.
+        """
+        required_vars = ["GOOGLE_API_KEY","GROQ_API_KEY"]
+        self.api_keys = {key: os.getenv(key) for key in required_vars}
+        missing = [key for key, value in self.api_keys.items() if not key]
+        if missing:
+            log.error("Missing environment variables", missing_vars=missing)
+            raise DocumentPortalException(f"Missing environment variables", sys)
+        log.info("Environment variables validated", api_keys=self.api_keys)
+            
+    def load_embedding_model(self):
+        """
+        Load the embedding model.
+        """
+        pass
+    
+    def load_llm(self):
+        pass
+    
+    
